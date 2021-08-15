bd3cfd1e46cc5562dc5552266781dfe8617bd6b3
Paul Brannan <cout@users.noreply.github.com>
2021-08-14 20:58:37 -0400

    Add MemoryMixin to reduce code duplication

--- memory.py
+++ memory.py
@@ -1,4 +1,17 @@
-class MemoryRegion(object):
+class MemoryMixin(object):
+  def short(self, addr):
+    lo = self[addr] or 0
+    hi = self[addr + 1] or 0
+    return lo | hi << 8
+
+  def bignum(self, addr, size):
+    result = 0
+    for i in range(0, size):
+      octet = self[addr + i] or 0
+      result |= octet << (8 * i)
+    return result
+
+class MemoryRegion(MemoryMixin):
   def __init__(self, addr, s):
     self.start = addr
     self.s = s
@@ -14,19 +27,7 @@ class MemoryRegion(object):
   def __len__(self):
     return len(self.s)
 
-  def short(self, addr):
-    lo = self[addr] or 0
-    hi = self[addr + 1] or 0
-    return lo | hi << 8
-
-  def bignum(self, addr, size):
-    result = 0
-    for i in range(0, size):
-      octet = self[addr + i] or 0
-      result |= octet << (8 * i)
-    return result
-
-class SparseMemory(object):
+class SparseMemory(MemoryMixin):
   def __init__(self, *regions):
     self.regions = regions
 
@@ -36,15 +37,3 @@ class SparseMemory(object):
         return region[addr]
 
     raise IndexError("address out of range")
-
-  def short(self, addr):
-    lo = self[addr] or 0
-    hi = self[addr + 1] or 0
-    return lo | hi << 8
-
-  def bignum(self, addr, size):
-    result = 0
-    for i in range(0, size):
-      octet = self[addr + i] or 0
-      result |= octet << (8 * i)
-    return result
